2c680df3196fa48110b3a1a60b39ab2d5572c228
Aliou20 <aliou20dev@gmail.com>
2022-11-24 18:48:22 +0000

    Ajouter mon de application météo

---
+++ src/Components/Meteo/Meteo.jsx
@@ -0,0 +1,12 @@
+import "./Meteo.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="meteo">
+      <h1>Meteo</h1>
+    </div>
+  );
+};
+
+export default template;
